d24558469857799f981fe581d8cb8b5adcf691ed
Chris Cardone <corrado-cristopher.cardone@atos.net>
2023-11-15 23:28:29 +0100

    auth flow && Navbar Component && home page general layout done

---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+export default function NavBar() {
+  return (
+    <nav className="p-4 shadow">
+      <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between">
+        <Link href="/notes" className="flex items-center gap-1">
+          <Image src={logo} alt="Brain logo" width={40} height={40} />
+          <span className="font-bold">Brain</span>
+        </Link>
+        <div className="flex items-center gap-2">
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+            }}
+          />
+          <Button>
+            <Plus size={20} className="mr-2"/>
+            Add Note
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+}
