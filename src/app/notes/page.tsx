aa03e6e14cbfd30ad43965e0e32811afda76ca8a
Chris Cardone <corrado-cristopher.cardone@atos.net>
2023-11-15 22:49:13 +0100

    Clerk authentication added && metadata to pages added

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata:Metadata = {
+    title:"Brain - Notes"
+}
+
+
+export default function NotesPage(){
+    return <div>Here the notes....</div>
+}
---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+    title:"Brain - Sign In"
+}
+ 
+export default function Page() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+        <SignIn appearance={{variables:{colorPrimary:'#0F172A'}}} />
+    </div>
+  ) 
+}
