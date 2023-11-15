a761c290c486c4aade417a5005822e827b958179
Chris Cardone <corrado-cristopher.cardone@atos.net>
2023-11-15 23:48:39 +0100

    note route added

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,10 +1,16 @@
-import { Metadata } from "next"
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
 
-export const metadata:Metadata = {
-    title:"Brain - Notes"
-}
+export const metadata: Metadata = {
+  title: "Brain - Notes",
+};
 
+export default async function NotesPage() {
+  const { userId } = auth();
 
-export default function NotesPage(){
-    return <div>Here the notes....</div>
+  if (!userId) throw new Error("userId is undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+  return <div>{JSON.stringify(allNotes)}</div>;
 }
